24aff51f5f64582f4051b18fe89627c760affba0
Muhammad Nabiel Andityo <mnabielap@gmail.com>
2023-11-28 17:09:52 +0700

    handle api error

--- project_django/views.py
+++ project_django/views.py
@@ -20,55 +20,76 @@ def api_from_machine(request):
         code_raw = data['code_loker']
         command = str(code_raw).split("_")[0]
         uuid_code = uuid.UUID(str(code_raw).split("_")[-1])
-        transaksi = TransaksiPeminjaman.objects.filter(uuid_code=uuid_code)[0]
+        try:
+            transaksi = TransaksiPeminjaman.objects.filter(uuid_code=uuid_code)[0]
+        except:
+            resp = {
+                'success':False,
+                'message':f'Tidak ditemukan transaksi dengan ID {uuid_code}',
+            }
+            return JsonResponse(resp, status = 400)
         if command == 'O':
-            resp = {
-                'success':True,
-                'message':f'Sukses memulai peminjaman pada loker nomor {transaksi.loker.nomor_loker}',
-            }
-            transaksi.is_scanned_open = True
-            transaksi.mulaipinjam = timezone.now()
-            transaksi.save()
-            return JsonResponse(resp, status = 200)
-        elif command == 'C':
-            resp = {
-                'success':True,
-                'message':f'Sukses menyelesaikan peminjaman pada loker nomor {transaksi.loker.nomor_loker}',
-            }
-            transaksi.is_scanned_close = True
-            transaksi.akhirpinjam = timezone.now()
-            transaksi.status = "FINISHED"
-            selisih_waktu = transaksi.akhirpinjam - transaksi.mulaipinjam
-            selisih_15min = math.ceil(selisih_waktu.total_seconds() / 900)
-            transaksi.total_harga = transaksi.loker.grup_loker.harga_loker * selisih_15min
-            transaksi.save()
-
-            pengguna_obj = get_object_or_404(Pengguna, user=transaksi.pengguna.user) # mendapatkan object pengguna
-            loker = get_object_or_404(Loker, id=transaksi.loker.id)   # user.getLokerFromId(loker_id)
-
-            if str(transaksi.pengguna.user.username).endswith("@ui.ac.id"):
-                today = datetime.date.today()
-                transaksi_peminjaman_hari_ini = TransaksiPeminjaman.objects.filter(
-                    pengguna=pengguna_obj,
-                    loker=loker,
-                    status="FINISHED",
-                    mulaipinjam__date=today,
-                    akhirpinjam__date=today
-                ).order_by('mulaipinjam')
-                new_saldo = sum([transaksi.total_harga for transaksi in transaksi_peminjaman_hari_ini])
-                curr_15m = 0
-                for i, transaksi in enumerate(transaksi_peminjaman_hari_ini):
-                    curr_15m += (transaksi.total_harga//transaksi.loker.grup_loker.harga_loker)
-                    if (curr_15m >= 8):
-                        lebih = curr_15m - 8
-                        new_saldo -= transaksi.loker.grup_loker.harga_loker*lebih
-                        break
-                    new_saldo -= transaksi.total_harga
-                pengguna_obj.saldo = new_saldo # pengguna_obj.setSaldo(new_saldo)
+            if (not transaksi.is_scanned_open):
+                transaksi.is_scanned_open = True
+                transaksi.mulaipinjam = timezone.now()
+                transaksi.save()
+                resp = {
+                    'success':True,
+                    'message':f'Sukses memulai peminjaman pada loker nomor {transaksi.loker.nomor_loker}',
+                }
+                return JsonResponse(resp, status = 200)
             else:
-                pengguna_obj.saldo -= transaksi.total_harga # pengguna_obj.reduceBalance(transaksi_peminjaman.getTotalHarga())
-            pengguna_obj.save()
+                resp = {
+                    'success':False,
+                    'message':f'Sudah pernah di scan transaksi peminjaman dengan ID {uuid_code}',
+                }
+                return JsonResponse(resp, status = 400)
+        elif command == 'C':
+            if (not transaksi.is_scanned_close):
+                transaksi.is_scanned_close = True
+                transaksi.akhirpinjam = timezone.now()
+                transaksi.status = "FINISHED"
+                selisih_waktu = transaksi.akhirpinjam - transaksi.mulaipinjam
+                selisih_15min = math.ceil(selisih_waktu.total_seconds() / 900)
+                transaksi.total_harga = transaksi.loker.grup_loker.harga_loker * selisih_15min
+                transaksi.save()
 
-            return JsonResponse(resp, status = 200)
+                pengguna_obj = get_object_or_404(Pengguna, user=transaksi.pengguna.user) # mendapatkan object pengguna
+                loker = get_object_or_404(Loker, id=transaksi.loker.id)   # user.getLokerFromId(loker_id)
+
+                if str(transaksi.pengguna.user.username).endswith("@ui.ac.id"):
+                    today = datetime.date.today()
+                    transaksi_peminjaman_hari_ini = TransaksiPeminjaman.objects.filter(
+                        pengguna=pengguna_obj,
+                        loker=loker,
+                        status="FINISHED",
+                        mulaipinjam__date=today,
+                        akhirpinjam__date=today
+                    ).order_by('mulaipinjam')
+                    new_saldo = sum([transaksi.total_harga for transaksi in transaksi_peminjaman_hari_ini])
+                    curr_15m = 0
+                    for i, transaksi in enumerate(transaksi_peminjaman_hari_ini):
+                        curr_15m += (transaksi.total_harga//transaksi.loker.grup_loker.harga_loker)
+                        if (curr_15m >= 8):
+                            lebih = curr_15m - 8
+                            new_saldo -= transaksi.loker.grup_loker.harga_loker*lebih
+                            break
+                        new_saldo -= transaksi.total_harga
+                    pengguna_obj.saldo = new_saldo # pengguna_obj.setSaldo(new_saldo)
+                else:
+                    pengguna_obj.saldo -= transaksi.total_harga # pengguna_obj.reduceBalance(transaksi_peminjaman.getTotalHarga())
+                pengguna_obj.save()
+                resp = {
+                    'success':True,
+                    'message':f'Sukses menyelesaikan peminjaman pada loker nomor {transaksi.loker.nomor_loker}',
+                }
+                return JsonResponse(resp, status = 200)
+            else:
+                resp = {
+                    'success':False,
+                    'message':f'Sudah pernah di scan transaksi pengembalian dengan ID {uuid_code}',
+                }
+                return JsonResponse(resp, status = 400)
+
     else:
         return JsonResponse({"status": "error"}, status = 401)
